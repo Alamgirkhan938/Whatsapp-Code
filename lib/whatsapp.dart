6d1361137c2928540ec7fff15f9c724ca18d6daa
Alamgir khan <alamgirkhanmarwat6@gmail.com>
2024-06-19 06:58:09 -0700

    change color

--- lib/whatsapp.dart
+++ lib/whatsapp.dart
@@ -14,7 +14,7 @@ class _WhatsappState extends State<Whatsapp> {
       length: 4,
       child: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.greenAccent,
+          backgroundColor: Colors.pink,
           title: Text('WhatsApp',style: TextStyle(fontWeight: FontWeight.bold),),
           actions: [
             PopupMenuButton(
